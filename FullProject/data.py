9034bf093e958560fe05bf7f18ea4f981bf47c89
Derrick Osei Acheampong <63465927+Owoahene-ik@users.noreply.github.com>
2020-06-06 23:35:57 +0000

    Add files via upload

---
+++ FullProject/data.py
@@ -0,0 +1,20 @@
+def article():
+    articles = [
+        {
+            'num': 1,
+            'title' : 'First article',
+            'details' : 'This is my first flask web app and is pretty simple to design',
+            'author': 'Owoahene',
+            'create_date': '02-may-2020'
+            
+        },
+         {
+            'num': 2,
+            'title' : 'Second article',
+            'details' : 'This is my first flask web app and is pretty simple to design',
+            'author': 'Akwasi',
+            'create_date': '20-may-2020'
+            
+        }
+    ]
+    return articles
--- FullProject/flask_app.py
+++ FullProject/flask_app.py
@@ -1,11 +1,28 @@
 from flask import Flask,render_template
+from data import article
 
 app = Flask(__name__)
 
-@app.route('/')
+article = article()
 
+
+@app.route("/")
 def index():
     return render_template("home.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/articles')
+def articles():
+    return render_template("articles.html",articles=article)
+
+@app.route('/test/<string:num>')
+def test(num):
+    return render_template("test.html", num=num)
+
 
 
 if __name__== '__main__':
